2f93306fb1b16e54d3fcf2f7062a1bb30e3bb6da
dostonradjabov <radjabovdoston0809@gmail.com>
2023-11-06 18:40:06 +0500

    .

--- While04.py
+++ While04.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    upper=0
+    while i<len(s):
+        if s[i].islower():
+            lower+=1
+        i+=1
+    return upper
+print(main("CodeschoolUz"))
